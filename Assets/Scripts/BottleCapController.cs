4e9ff500f88ff9566eca358d12317b7320f14a04
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-10-18 18:06:54 +0900

    [FEAT] Bottle Drag

--- Assets/Scripts/BottleCapController.cs
+++ Assets/Scripts/BottleCapController.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class BottleCapController : MonoBehaviour
 {
     public Rigidbody rigd;
+    public Camera cam;
 
-    private Vector2 dragStartPos;
-    private Vector2 dragEndPos;
+    private Vector3 dragStartPos;
+    private Vector3 dragEndPos;
 
     public float power = 10f;
+
 
     void Update()
     {
         //마우스를 누르고 있을때
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            dragStartPos = Input.mousePosition;
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = cam.WorldToScreenPoint(transform.position).z;
+            dragStartPos = cam.ScreenToWorldPoint(mousePos);
             Debug.Log("dragStartPos : " + dragStartPos);
         }
-
         //마우스를 뗐을때
         if (Input.GetMouseButtonUp(0))
         {
-            dragEndPos = Input.mousePosition;
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = cam.WorldToScreenPoint(transform.position).z;
+            dragEndPos = cam.ScreenToWorldPoint(mousePos);
             Debug.Log("dragEndPos : " + dragEndPos);
             Shoot();
         }
@@ -32,12 +37,12 @@ public class BottleCapController : MonoBehaviour
     void Shoot()
     {
         //드래그한 값을 빼서 넣어줌
-        Vector2 dragVector = dragEndPos - dragStartPos;
+        Vector3 dragVector = dragEndPos - dragStartPos;
         //드래그한 값의 x, y축 좌표를 가져와줌
-        Vector3 vec = new Vector3(dragVector.x, 0, dragVector.y);
+        Vector3 vec = new Vector3(dragVector.x,0, dragVector.z);
 
-        //x, y축 * 힘만큼 앞으로 이동
-        rigd.AddForce(vec * power);
+        //x * 힘만큼 앞으로 이동
+        rigd.AddForce(vec * power*-1);
     }
 
 }
